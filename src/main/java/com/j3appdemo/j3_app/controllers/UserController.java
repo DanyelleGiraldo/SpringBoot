8d08948d0655e6b1c402b5ae0a22cdef2d437c1c
DanyelleGiraldo <152023740+DanyelleGiraldo@users.noreply.github.com>
2024-08-06 17:01:47 -0500

    Add files via upload

--- src/main/java/com/j3appdemo/j3_app/controllers/UserController.java
+++ src/main/java/com/j3appdemo/j3_app/controllers/UserController.java
@@ -1,19 +1,39 @@
 package com.j3appdemo.j3_app.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.j3appdemo.j3_app.models.User;
+
 
 @Controller
 public class UserController {
     @GetMapping("/details")
     public String details(Model model){
-    User user = new User("Daniel","Giraldo");
-    model.addAttribute("title", "Hola");
-    model.addAttribute("user", user);
-    return "details";
+        User user = new User("Daniel","Giraldo", "");
+        user.setEmail("danyellesgiraldoj@gmail.com");
+        model.addAttribute("title", "Hola");
+        model.addAttribute("user", user);
+        return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(
+            new User("Danilo", "Giraldo", "danyelle@gmail.com"),
+            new User("Martha", "Sanchez", "martha@gmail.com"),
+            new User("Vicente", "camargo","vicente@gmail.com"),
+            new User("Danyelle", "Giraldo")
+        );
+        model.addAttribute("title", "Listado de usuarios");
+        model.addAttribute("users", users);
+        return "list";
+    }
+    
 
 }
--- src/main/java/com/j3appdemo/j3_app/controllers/UserRestController.java
+++ src/main/java/com/j3appdemo/j3_app/controllers/UserRestController.java
@@ -1,25 +1,51 @@
 package com.j3appdemo.j3_app.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.j3appdemo.j3_app.models.User;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.j3appdemo.j3_app.models.User;
+import com.j3appdemo.j3_app.models.dto.UserDto;
+
+
 
 
 @RestController
 @RequestMapping("/v2/api")
 public class UserRestController {
+
     @GetMapping("/details")
-    public Map<String,Object> details(){
-        User user = new User("Danyelle", "Giraldo");
-        Map<String,Object> body = new HashMap<>();
-        body.put("title", "Desarrollando con Spring boot CreativeCode");
-        body.put("user", user);
-        return body;
-    }    
+    public UserDto details() {
+        UserDto userDto = new UserDto();
+        User user = new User("Danyelle", "Giraldo", "danyellesgiraldoj@gmail.com");
+        userDto.setTitle("Desarrollando con spring:D");
+        userDto.setUser(user);
+        return userDto;
+    }
+
+    @GetMapping("/list-details")
+    public List<User> listdetails() {
+        User user = new User("Danilo", "Jimenez", "");
+        User userA = new User("Juan", "Tinoco", "" );
+        User userB = new User("Dylan","Giraldo", "");
+        List<User> lstUsers= new ArrayList<>();
+        lstUsers.add(user);
+        lstUsers.add(userA);
+        lstUsers.add(userB);
+        return lstUsers;
+    }
+    
+    
+
+    // @GetMapping("/details")
+    // public Map<String,Object> details(){
+    //     User user = new User("Danyelle", "Giraldo");
+    //     Map<String,Object> body = new HashMap<>();
+    //     body.put("title", "Desarrollando con Spring boot CreativeCode");
+    //     body.put("user", user);
+    //     return body;
+    // }    
 }
